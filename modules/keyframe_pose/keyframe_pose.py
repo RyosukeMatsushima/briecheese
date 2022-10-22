3c379663e83c96d5be316db7b1a968e8249ebe15
RyosukeMatsushima <m.ryosuke@me.com>
2022-10-22 14:10:50 +0900

    add keyframe_pose module

---
+++ modules/keyframe_pose/keyframe_pose.py
@@ -0,0 +1,30 @@
+
+
+class KeyframePose:
+
+
+
+
+    # keyframeの位置を知るために必要なこと
+    # - 位置が既知のfeature points
+    # - それらを含むbundles
+
+    def print_a(self):
+        print('a')
+
+    # bundles: [ [ feature_point_id, direction ], ... ]
+    def get_pose(self, feature_point_directions):
+
+
+        # optimizer に位置が既知のfeature points をセット
+        # optimizer にfeature point directions をセット
+        # optimiser.optimise(self.print_a)
+
+        # 
+
+
+
+
+
+
+
