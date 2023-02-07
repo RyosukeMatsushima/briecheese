d08dc1c2058acd8ca7241d1b566aac1d617c0f1b
RyosukeMatsushima <m.ryosuke@me.com>
2023-02-08 07:58:25 +0900

    add main.py

---
+++ modules/main/main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from modules.feature_point_id.feature_point_id import FeaturePointId
+from modules.featrue_point_direction.featrue_point_direction import FeaturePointDirection
+from modules.feature_point_positions.feature_point_positions import FeaturePointPositions
+from modules.keyframe_pose.keyframe_pose import KeyframePose
+
+class Main:
+
+    def __init__(self, fx, fy, cx, cy):
+
+        self.featurePointId = FeaturePointId()
+        self.featurePointDirection = FeaturePointDirection(fx, fy, cx, cy)
+        self.featurePointPositions = FeaturePointPositions()
+        self.keyframePose = KeyframePose()
+
+    def add_frame(self, frame, observed_position, observed_rotation):
+
+        feature_point_directions = self.get_feature_point_directions(frame)
+        self.featurePointPositions.add_keyframe(observed_position, observed_rotation, feature_point_directions)
+
+    def get_pose(self, frame):
+
+        feature_point_directions = self.get_feature_point_directions(frame)
+        position, ratation = self.keyframePose.get_pose(feature_point_directions)
+        return position, rotation
+
+    def get_feature_point_directions(self, frame):
+        feature_point_pixels = self.featurePointId.get_with_pixel(frame)
+
+        feature_point_directions = []
+
+        for feature_point_pixel in feature_point_pixels:
+            feature_point_direction = self.featurePointDirection.get(feature_point_pixel[0][0], feature_point_pixel[0][1])
+
+            feature_point_directions.append([feature_point_pixel[1], feature_point_direction])
+
+        return feature_point_directions
+
+
