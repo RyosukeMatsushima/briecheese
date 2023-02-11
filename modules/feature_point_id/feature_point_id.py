77441cdc0e8134a455b4e817b6c57caf5841af39
RyosukeMatsushima <m.ryosuke@me.com>
2023-02-11 23:28:18 +0900

    dbへの保存回数が多すぎるため
    前後のフレームで一致した特徴点のみ保存

--- modules/feature_point_id/feature_point_id.py
+++ modules/feature_point_id/feature_point_id.py
@@ -14,6 +14,7 @@ class FeaturePointId:
         )
         self.detector = cv.ORB_create(nfeatures=1000)
         self.matcher = cv.FlannBasedMatcher(flann_params, {})
+        self.matcher_wit_latest = None
         self.db = DescriptorsDB()
         db_descriptors = self.db.get_all()
         descriptors = []
@@ -31,6 +32,8 @@ class FeaturePointId:
         ]
         response = []
         match_keypoints = []
+        match_query_indexes = []
+
         for m in matches:
             feature_point_id_column = self.db.find_by_descriptor(
                 json.dumps(self.matcher.getTrainDescriptors()[0][m.trainIdx].tolist())
@@ -38,17 +41,39 @@ class FeaturePointId:
             response.append([keypoints[m.queryIdx].pt, feature_point_id_column[0]])
             match_keypoints.append(keypoints[m.queryIdx])
 
-        all_descriptors = list(zip(keypoints, descriptors))
-        add_matcher_descriptors = []
-        for descriptors in all_descriptors:
-            if not descriptors[0] in match_keypoints:
-                feature_point_id_column = self.db.create(descriptors[1].tolist())
-                response.append([descriptors[0].pt, feature_point_id_column[0]])
-                add_matcher_descriptors.append(np.uint8(descriptors[1]))
-        if add_matcher_descriptors:
-            self.matcher.add(np.asarray([add_matcher_descriptors]))
+            match_query_indexes.append(m.queryIdx)
 
+        unregistered_descriptors = np.delete(descriptors, match_query_indexes, 0)
+        descriptors_to_regist = self.update_matcher_with_latest(unregistered_descriptors)
+
+        for descriptor in descriptors_to_regist:
+            feature_point_id_column = self.db.create(descriptor.tolist())
+        if descriptors_to_regist:
+            self.matcher.add(np.asarray([descriptors_to_regist]))
+
+        keypoints, descriptors = self.detectAndCompute(frame)
         return response
+
+    def update_matcher_with_latest(self, unregistered_descriptors):
+
+        descriptors_to_regist = []
+
+        if self.matcher_wit_latest:
+            matches = self.matcher_wit_latest.knnMatch(unregistered_descriptors, k=2)
+            matches = [
+                m[0] for m in matches if len(m) == 2 and m[0].distance < m[1].distance * 0.5
+            ]
+
+            for m in matches:
+                descriptors_to_regist.append(self.matcher_wit_latest.getTrainDescriptors()[0][m.trainIdx])
+
+        flann_params = dict(
+            algorithm=6, table_number=6, key_size=12, multi_probe_level=1
+        )
+        self.matcher_wit_latest = cv.FlannBasedMatcher(flann_params, {})
+        self.matcher_wit_latest.add(np.asarray([unregistered_descriptors]))
+
+        return descriptors_to_regist
 
     def detectAndCompute(self, frame):
         keypoints, descriptors = self.detector.detectAndCompute(frame, None)
